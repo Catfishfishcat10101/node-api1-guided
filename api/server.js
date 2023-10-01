16a969e9fe7ffeba76fcbb3f255d94dd5ce1b1a9
William Johnson <catfishfishcat10101@gmail.com>
2023-10-01 04:10:03 -0500

    finished CRUD

--- api/server.js
+++ api/server.js
@@ -65,11 +65,23 @@ server.put('api/dogs/:id', async (req, res) => {
     try {
         const { id } = req.params
         const { name, weight }= req.body
+        if (!name || !weight) {
+            res.status(422).json({
+                message: 'dogs need name and weight'
+            })
+        } else {
         const updateDog = await Dog.update(id, { name, weight })
-        res.status(200).json({
-            message: `dog was updated successfully`,
-            data: updatedDog,
-        })
+        if (!updatedDog) {
+            res.status(404).json({
+                message: `dog ${id} not found, sorry`
+            })
+        } else {
+            res.status(200).json({
+                message: `dog was updated successfully`,
+                data: updatedDog,
+            })
+        }
+        }
 
     } catch (err) {
         res.status(500).json({
